c5bf5ea796e3ebcafee94910f0b6b84fae8bde28
Alejandro Sanchez <as.techseven@gmail.com>
2022-08-25 02:02:36 -0400

    feat(install dependencies): install security, jwt & performances

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  app.use(helmet());
+
+  await app.listen(parseInt(process.env.APP_PORT), '0.0.0.0');
+  console.log('Listen in port: ', process.env.APP_PORT);
 }
 bootstrap();
